12ed42df4f228dc626f9b1bab66ee0c98b923177
LightspeedLazer <michael@the-sims-family.net>
2024-01-20 17:57:18 -0500

    Note 2d poses

--- src/main/java/frc/robot/subsystems/vision/note/NoteVision.java
+++ src/main/java/frc/robot/subsystems/vision/note/NoteVision.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.PhotonCamera;
 
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import frc.robot.RobotState;
 import frc.robot.util.VirtualSubsystem;
 
 public class NoteVision extends VirtualSubsystem {
@@ -25,11 +27,29 @@ public class NoteVision extends VirtualSubsystem {
 
         Logger.recordOutput("Vision/pitch", photonTargets.stream().map((target) -> new Translation2d(target.getPitch(), target.getYaw())).toArray(Translation2d[]::new));
         Logger.recordOutput("Vision/Best Cam to Targets", photonTargets.stream().map((target) -> robotToCam.plus(new Transform3d(new Translation3d(), new Rotation3d(0, Units.degreesToRadians(-target.getPitch()), Units.degreesToRadians(-target.getYaw()))))).toArray(Transform3d[]::new));
-        // Logger.recordOutput("Vision/Targets from Trig", photonTargets.stream().map(
-        //     (target) -> {
-        //         target.get
-        //     }
-        // ).toArray(Translation2d[]::new));
+        Logger.recordOutput("Vision/Targets from Trig", photonTargets.stream().map(
+            (target) -> robotToCam.plus(
+                new Transform3d(
+                    new Translation3d(),
+                    new Rotation3d(
+                        0,
+                        Units.degreesToRadians(-target.getPitch()),
+                        Units.degreesToRadians(-target.getYaw())
+                    )
+                )
+            )
+        ).map(
+            (targetCamViewTransform) -> {
+                var distOut = targetCamViewTransform.getTranslation().getZ() / Math.tan(targetCamViewTransform.getRotation().getY());
+                var distOff = distOut * Math.tan(targetCamViewTransform.getRotation().getZ());
+                return new Translation3d(distOut, distOff, -robotToCam.getZ());
+            }
+        ).map(
+            (camToTargetTranslation) -> 
+                new Pose3d(RobotState.getInstance().getPose()).transformBy(robotToCam).transformBy(new Transform3d(camToTargetTranslation, new Rotation3d()))
+        )/* .map(
+            (target3dPose) -> target3dPose.toPose2d().getTranslation()
+        ) */.toArray(Pose3d[]::new));
         // for(var target : photonTargets) {
         // }
     }
