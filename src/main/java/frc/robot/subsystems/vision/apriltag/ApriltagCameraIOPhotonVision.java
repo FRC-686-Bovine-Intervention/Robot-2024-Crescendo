023e00888d485b3f9bb07a3085a557290614fd28
LightspeedLazer <michael@the-sims-family.net>
2024-02-08 19:31:34 -0500

    Loads Crescendo field layout

--- src/main/java/frc/robot/subsystems/vision/apriltag/ApriltagCameraIOPhotonVision.java
+++ src/main/java/frc/robot/subsystems/vision/apriltag/ApriltagCameraIOPhotonVision.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.vision.apriltag;
 
-import java.io.IOException;
 import java.util.Optional;
 
 import org.photonvision.PhotonCamera;
@@ -8,8 +7,8 @@ import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants.VisionConstants.Camera;
 import frc.robot.RobotState;
@@ -26,8 +25,8 @@ public class ApriltagCameraIOPhotonVision implements ApriltagCameraIO {
 
         try {
             // Attempt to load the AprilTagFieldLayout that will tell us where the tags are on the field.
-            String resourceFile = Filesystem.getDeployDirectory() + "/Bunnybots_2023.json";
-            AprilTagFieldLayout fieldLayout = new AprilTagFieldLayout(resourceFile);
+            // String resourceFile = Filesystem.getDeployDirectory() + "/Bunnybots_2023.json";
+            AprilTagFieldLayout fieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
 
             // test that I made the JSON file correctly
             // List<AprilTag> tags = fieldLayout.getTags();
@@ -38,7 +37,7 @@ public class ApriltagCameraIOPhotonVision implements ApriltagCameraIO {
 
             photonPoseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, photonCam, cam.getRobotToCam());
             photonPoseEstimator.setMultiTagFallbackStrategy(PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY);
-        } catch (IOException e) {
+        } catch (Exception e) {
             // The AprilTagFieldLayout failed to load. We won't be able to estimate poses if we don't know
             // where the tags are.
             DriverStation.reportError("Failed to load AprilTagFieldLayout", e.getStackTrace());
