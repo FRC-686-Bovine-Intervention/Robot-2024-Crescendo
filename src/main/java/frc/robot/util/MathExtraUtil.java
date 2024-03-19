413e8e5a86aa22fc1031edfc2d4fa9f6570ea639
Michael Sims <michael@the-sims-family.net>
2024-03-19 01:00:46 -0400

    removed MathExtraUtil logging

--- src/main/java/frc/robot/util/MathExtraUtil.java
+++ src/main/java/frc/robot/util/MathExtraUtil.java
@@ -2,8 +2,6 @@ package frc.robot.util;
 
 import java.util.Arrays;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -17,9 +15,6 @@ public class MathExtraUtil {
 
     public static boolean isNear(Pose2d expected, Pose2d actual, double linearTolerance, double angularTolerance) {
         var bol = isNear(expected.getTranslation(), actual.getTranslation(), linearTolerance) && isNear(expected.getRotation(), actual.getRotation(), angularTolerance);
-        Logger.recordOutput("DEBUG/Pos/IsNear", bol);
-        Logger.recordOutput("DEBUG/Pos/Expected", expected);
-        Logger.recordOutput("DEBUG/Pos/Actual", actual);
         return bol;
     }
     public static boolean isNear(Translation2d expected, Translation2d actual, double tolerance) {
@@ -30,9 +25,6 @@ public class MathExtraUtil {
     }
     public static boolean isNear(ChassisSpeeds expected, ChassisSpeeds actual, double linearTolerance, double angularTolerance) {
         var bol = isNear(new Translation2d(expected.vxMetersPerSecond, expected.vyMetersPerSecond), new Translation2d(actual.vxMetersPerSecond, actual.vyMetersPerSecond), linearTolerance) && MathUtil.isNear(expected.omegaRadiansPerSecond, actual.omegaRadiansPerSecond, angularTolerance);
-        Logger.recordOutput("DEBUG/Speed/IsNear", bol);
-        Logger.recordOutput("DEBUG/Speed/Expected", expected);
-        Logger.recordOutput("DEBUG/Speed/Actual", actual);
         return bol;
     }
 }
